51e61a54d9defa46e11a3fbb9383cc0a0ce5d7d7
Maksim <kodermax@gmail.com>
2023-11-11 00:30:32 +0300

    up

--- dist/server/routes/index.js
+++ dist/server/routes/index.js
@@ -3,10 +3,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = [
     {
         method: 'GET',
-        path: '/',
-        handler: 'myController.index',
+        path: '/docs/:folderName',
+        handler: 'fs.getOne',
         config: {
             policies: [],
+            auth: false,
+        },
+    },
+    {
+        method: 'GET',
+        path: '/folders',
+        handler: 'fs.getAllFolders',
+        config: {
+            policies: [],
+            auth: false,
+        },
+    },
+    {
+        method: 'GET',
+        path: '/root-files',
+        handler: 'fs.getBaseFiles',
+        config: {
+            policies: [],
+            auth: false,
         },
     },
 ];
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,10 +1,29 @@
 export default [
   {
     method: 'GET',
-    path: '/',
-    handler: 'myController.index',
+    path: '/docs/:folderName',
+    handler: 'fs.getOne',
     config: {
       policies: [],
+      auth: false,
+    },
+  },
+  {
+    method: 'GET',
+    path: '/folders',
+    handler: 'fs.getAllFolders',
+    config: {
+      policies: [],
+      auth: false,
+    },
+  },
+  {
+    method: 'GET',
+    path: '/root-files',
+    handler: 'fs.getBaseFiles',
+    config: {
+      policies: [],
+      auth: false,
     },
   },
 ];
